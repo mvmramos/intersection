03fc9706c4d5eeae6fe22b07673cb17963204062
mvmramos <marcus.ramos@univasf.edu.br>
2017-04-09 15:44:16 -0300

    Add files via upload

---
+++ cfl.v
@@ -0,0 +1,199 @@
+(* ---------------------------------------------------------------------
+
+   This file is part of a repository containing the definitions and 
+   proof scripts related to the formalization of context-free language
+   theory in Coq. Specifically, the following results were obtained:
+   
+   (i) languages square, prime and anbncn are not context-free; 
+   (ii) context-free languages are not closed under intersection.
+   
+   More information can be found in the article "Applications of the 
+   Formalization of the Pumping Lemma for Context-Free Languages, 
+   submitted to ITP 2017". Also, in the thesis "Formalization of 
+   Context-Free Language Theory", submitted to the Informatics
+   Center of the Pernambuco Federal University (CIn/UFPE) in
+   Brazil.
+   
+   The file README.md describes the contents of each file and 
+   provides instructions on how to compile them.
+   
+   Marcus Vinícius Midena Ramos
+   mvmramos@gmail.com
+   --------------------------------------------------------------------- *)
+
+(* --------------------------------------------------------------------- *)
+(* CONTEXT FREE LANGUAGES                                                *)
+(* --------------------------------------------------------------------- *)
+
+Require Import List.
+Require Import Ring.
+Require Import Omega.
+
+Require Import misc_arith.
+Require Import misc_list.
+Require Import cfg.
+Require Import emptyrules.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Import ListNotations.
+Open Scope list_scope.
+
+(* --------------------------------------------------------------------- *)
+(* CONTEXT-FREE LANGUAGES - DEFINITIONS                                  *)
+(* --------------------------------------------------------------------- *)
+
+Section ContextFreeLanguages_Definitions.
+
+Variables non_terminal terminal: Type.
+
+Notation sentence:= (list terminal).
+ 
+Definition lang := sentence -> Prop.
+
+Definition lang_of_g (g: cfg non_terminal terminal): lang :=
+fun w: sentence => produces g w.
+
+Definition lang_eq (l k: lang) := 
+forall w, l w <-> k w.
+
+Definition empty_lang (l: lang): Prop:=
+~ exists w: sentence,
+l w.
+
+Definition non_empty_lang (l: lang): Prop:=
+exists w: sentence,
+l w.
+
+Definition contains_empty (l: lang): Prop:=
+l [].
+
+Definition contains_non_empty (l: lang): Prop:=
+exists w: sentence,
+l w /\ w <> [].
+
+End ContextFreeLanguages_Definitions.
+
+Infix "==" := lang_eq (at level 80). 
+
+Definition cfl (terminal: Type) (l: lang terminal): Prop:=
+exists non_terminal: Type, 
+exists g: cfg non_terminal terminal, 
+l == lang_of_g g.
+
+(* --------------------------------------------------------------------- *)
+(* CONTEXT-FREE LANGUAGES - LEMMAS                                       *)
+(* --------------------------------------------------------------------- *)
+
+Section ContextFreeLanguages_Lemma.
+
+Variables non_terminal non_terminal' terminal: Type.
+
+Lemma produces_empty_equiv_contains_empty:
+forall g: cfg non_terminal terminal,
+produces_empty g <-> contains_empty (lang_of_g g).
+Proof.
+intros g.
+unfold produces_empty.
+unfold contains_empty.
+unfold lang_of_g.
+split; auto.
+Qed.
+
+Lemma produces_non_empty_equiv_contains_non_empty:
+forall g: cfg non_terminal terminal,
+produces_non_empty g <-> contains_non_empty (lang_of_g g).
+Proof.
+intros g.
+unfold produces_non_empty.
+unfold contains_non_empty.
+unfold lang_of_g.
+split; auto.
+Qed.
+
+Lemma g_equiv_lang_eq:
+forall g: cfg non_terminal terminal,
+forall g': cfg non_terminal' terminal,
+g_equiv g' g -> lang_of_g g' == lang_of_g g.
+Proof.
+unfold g_equiv.
+unfold lang_of_g.
+unfold lang_eq.
+intros g g' H1.
+split.
+- intros H2.
+  apply H1.
+  exact H2.
+- intros H2.
+  apply H1.
+  exact H2.
+Qed.
+
+Lemma lang_eq_sym:
+forall l1 l2: lang terminal,
+l1 == l2 ->
+l2 == l1.
+Proof.
+unfold lang_eq.
+intros l1 l2 H1 w.
+split.
+- intros H2.
+  apply H1.
+  exact H2.
+- intros H2.
+  apply H1.
+  exact H2.
+Qed.
+
+Lemma lang_eq_trans:
+forall l1 l2 l3: lang terminal,
+l1 == l2 ->
+l2 == l3 ->
+l1 == l3.
+Proof.
+unfold lang_eq.
+intros l1 l2 l3 H1 H3 w.
+split.
+- intros H4.
+  apply H3.
+  apply H1.
+  exact H4.
+- intros H4.
+  apply H1.
+  apply H3.
+  exact H4.
+Qed.
+
+Lemma lang_eq_change_g:
+forall l: lang terminal,
+forall g: cfg non_terminal terminal,
+forall g': cfg non_terminal' terminal,
+l == lang_of_g g -> 
+g_equiv g' g ->
+l == lang_of_g g'.
+Proof.
+intros l g g' H1 H2.
+apply g_equiv_lang_eq in H2.
+apply lang_eq_trans with (l2 := lang_of_g g).
+- exact H1.
+- apply lang_eq_sym.
+  exact H2.
+Qed.
+
+Lemma cfl_lang_eq: 
+forall (l1 l2: lang terminal),
+cfl l1 -> l1==l2 -> cfl l2.
+Proof.
+intros l1 l2 H1 H2.
+unfold cfl in H1.
+destruct H1 as [nt1 [g1 H1]].
+exists nt1, g1.
+apply lang_eq_trans with (l2:=l1).
+- apply lang_eq_sym.
+  exact H2.
+- exact H1.
+Qed.
+
+End ContextFreeLanguages_Lemma.
---
+++ pumping_square.v
@@ -0,0 +1,230 @@
+(* ---------------------------------------------------------------------
+
+   This file is part of a repository containing the definitions and 
+   proof scripts related to the formalization of context-free language
+   theory in Coq. Specifically, the following results were obtained:
+   
+   (i) languages square, prime and anbncn are not context-free; 
+   (ii) context-free languages are not closed under intersection.
+   
+   More information can be found in the article "Applications of the 
+   Formalization of the Pumping Lemma for Context-Free Languages, 
+   submitted to ITP 2017". Also, in the thesis "Formalization of 
+   Context-Free Language Theory", submitted to the Informatics
+   Center of the Pernambuco Federal University (CIn/UFPE) in
+   Brazil.
+   
+   The file README.md describes the contents of each file and 
+   provides instructions on how to compile them.
+   
+   Marcus Vinícius Midena Ramos
+   mvmramos@gmail.com
+   --------------------------------------------------------------------- *)
+
+Require Import List.
+Require Import Ring.
+Require Import Omega.
+Require Import NPeano.
+Require Import NZPow.
+
+Require Import misc_arith.
+Require Import misc_list.
+Require Import cfg.
+Require Import cfl.
+Require Import pumping.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Import ListNotations.
+Open Scope list_scope.
+
+(* --------------------------------------------------------------------- *)
+(* PUMPING LEMMA EXAMPLE                                                 *)
+(* --------------------------------------------------------------------- *)
+
+Section Pumping_Square.
+
+Inductive terminal: Type:=
+| a.
+
+Definition square: lang terminal:=
+fun (s: list terminal) =>
+exists i: nat,
+length s = i*i.
+
+Fixpoint build (k: nat): list terminal:=
+match k with
+| 0 => []
+| S k => a :: build k
+end.
+
+Lemma build_length:
+forall k: nat,
+length (build k) = k.
+Proof.
+induction k.
+- simpl.
+  reflexivity.
+- simpl.
+  rewrite IHk.
+  reflexivity.
+Qed.
+
+Lemma not_square:
+forall n i: nat,
+n > i*i ->
+n < (i+1)*(i+1) ->
+~ exists j: nat,
+  n = j*j.
+Proof.
+intros n i H1 H2 H3.
+destruct H3 as [j H3].
+rewrite H3 in H1, H2.
+clear H3.
+destruct j.
+- simpl in H1.
+  apply lt_n_0 in H1. 
+  contradiction.
+- rewrite <- Nat.pow_2_r in H1.
+  rewrite <- Nat.pow_2_r in H1. 
+  apply Nat.pow_lt_mono_l_iff in H1.
+  + assert (H3: i<=j) by omega.
+    clear H1. 
+    replace (S j) with (j+1) in H2.
+    * rewrite <- Nat.pow_2_r in H2.
+      rewrite <- Nat.pow_2_r in H2.
+      {
+      apply Nat.pow_lt_mono_l_iff in H2.
+      - assert (H4: j<i) by omega.
+        clear H2. 
+        omega.
+      - auto.
+      }
+    * omega. 
+  + auto.
+Qed.
+
+Lemma square_plus_not_square:
+forall i j k,
+j>=1 ->
+j<=i ->
+~ i*i+j=k*k.
+Proof.
+intros i j k H1 H2 H3.
+assert (H4: i*i+j > i*i) by omega.
+assert (H5: i*i+j < (i+1)*(i+1)).
+  {
+  replace ((i + 1) * (i + 1)) with (S i * S i).
+  - simpl.
+    rewrite <- mult_n_Sm.
+    omega.
+  - replace (S i) with (i+1). 
+    + reflexivity.
+    + omega.
+  }
+apply not_square in H4. 
+- apply H4.
+  exists k.
+  exact H3. 
+- exact H5.
+Qed.
+
+Lemma sentence_exists:
+forall n: nat,
+exists w: list terminal,
+length w = n*n /\ square w.
+Proof.
+destruct n.
+- exists [].
+  split.
+  + simpl.
+    reflexivity.
+  + unfold square.
+    exists 0.
+    simpl.
+    reflexivity.
+- exists (build (n*n+n+n+1)).
+  split.
+  + rewrite build_length.
+    apply prod_eq.
+  + unfold square. 
+    exists (S n).
+    rewrite build_length.
+    apply prod_eq.
+Qed.  
+
+Lemma not_cfl_square: ~ cfl square.
+Proof.
+intros H1.
+assert (H2: contains_empty square).
+  {
+  unfold contains_empty.
+  unfold square.
+  exists 0.
+  simpl.
+  reflexivity.
+  }
+assert (H3: contains_non_empty square).
+  {
+  unfold contains_non_empty.
+  exists [a].
+  unfold square.
+  split.
+  - exists 1.
+    simpl. 
+    reflexivity.
+  - intros H3.
+    inversion H3.
+  }
+apply pumping_lemma in H1. 
+- destruct H1 as [n H1]. 
+  assert (H4: exists (s: list terminal), (length s = n*n) /\ square s).
+    {
+    apply sentence_exists.
+    }
+  destruct H4 as [s [H4 H5]].
+  assert (H98: n*n >= n).
+    {
+    apply mult_ge.
+    }
+  assert (H6: length s >= n) by omega.
+  specialize (H1 s H5 H6).
+  destruct H1 as [u [v [w [x [y [H11 [H12 [H13 [H14 H15]]]]]]]]].
+  clear H2 H3 H6 H98.
+  specialize (H15 2).
+  simpl in H15.
+  repeat rewrite app_nil_r in H15.
+  unfold square in H15.
+  rewrite H11 in H4.
+  destruct H15 as [i H15].
+  repeat rewrite app_length in H15.
+  repeat rewrite app_length in H4.
+  replace (length u + (length v + length v + (length w + (length x + length x + length y)))) with
+          ((length u + length v + length w + length x + length y) + length v + length x) in H15.
+  + replace (length u + length v + length w + length x + length y + length v + length x) with
+            (length u + (length v + (length w + (length x + length y))) + (length v + length x)) in H15.
+    * rewrite H4 in H15.
+      clear H4.
+      {
+      replace (length v + length x) with (length (v++x)) in H15.
+      - apply square_plus_not_square in H15. 
+        + contradiction. 
+        + exact H12.
+        + rewrite app_length.
+          repeat rewrite app_length in H14.
+          omega.
+      - rewrite app_length.
+        reflexivity.
+      }
+    * omega.
+  + omega.
+- split.
+  + left.
+    exact H2.
+  + left.
+    exact H3.
+Qed.
+
+End Pumping_Square.
